bf9814601ee920975d95ae2af414c6c603bc9ca1
Jeffrey Seneff <jeffsnff@gmail.com>
2026-02-15 19:59:15 -0500

    Updates CreatBoarder to have the = as default. Removed commented out code that is not needed. Made the Status display more compact. Creates CharacterInfo method that displays member states

--- FinalBattle/Status.cs
+++ FinalBattle/Status.cs
@@ -10,7 +10,7 @@ public static class Status
   public static List<Character> Enemies { get; set; }
   public static Character currentPlayer { get; set; }
 
-  private static void CreateBorder(string symbol, string word = "")
+  private static void CreateBorder(string symbol = "=", string word = "")
   {
     string border = "";
     string title = word;
@@ -19,12 +19,30 @@ public static class Status
       border = border + symbol;
       if (border.Length == (Width / 2))
       {
-        // border = border.Remove((border.Length - title.Length));
         border = border + title;
         i = i + title.Length;
       }
     }
-    Console.WriteLine($"\n{border}\n");
+    Console.WriteLine($"{border}");
+  }
+
+  private static void CharacterInfo(Character member)
+  {
+    string text = $"{member.Name} ({member.Health}/{member.MaxHP})";
+    if (Enemies.Contains(member))
+    {
+      Console.CursorLeft = Width - (text.Length + 1);
+    }
+    if (member == currentPlayer)
+    {
+      Console.ForegroundColor = ConsoleColor.Green;
+      Console.WriteLine(text);
+      Console.ResetColor();
+    }
+    else
+    {
+      Console.WriteLine(text);
+    }
   }
 
   public static void BattleStatus()
@@ -32,35 +50,13 @@ public static class Status
     CreateBorder("=", " BATTLE ");
     foreach (Character member in Heros)
     {
-      if(member == currentPlayer)
-      {
-        Console.ForegroundColor = ConsoleColor.Green;
-        Console.WriteLine($"{member.Name} ({member.Health}/{member.MaxHP})");
-        Console.ResetColor();
-      }
-      else
-      {
-        Console.WriteLine($"{member.Name} ({member.Health}/{member.MaxHP})");
-      }
-      
+      CharacterInfo(member);
     }
     CreateBorder("-", " VS ");
     foreach (Character member in Enemies)
     {
-      string text = $"{member.Name} ({member.Health}/{member.MaxHP})";
-      Console.CursorLeft = Width - (text.Length) - 1;
-      if(member == currentPlayer)
-      {
-        Console.ForegroundColor = ConsoleColor.Green;
-        Console.WriteLine(text);
-        Console.ResetColor();
-      }
-      else
-      {
-        Console.WriteLine(text);
-      }
-      
+      CharacterInfo(member);
     }
-    CreateBorder("=");
+    CreateBorder();
   }
 }
