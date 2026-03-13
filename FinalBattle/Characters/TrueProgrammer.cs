3d3128262935663d62cae43cf7de8b54af95fc7f
Jeffrey Seneff <jeffsnff@gmail.com>
2026-03-13 12:28:45 -0400

    Updates Character.cs Name field to be abtract and public then changes Skeleton.cs, TrueProgrammer.cs, UnCodedOne.cs to implement that change.

--- FinalBattle/Characters/TrueProgrammer.cs
+++ FinalBattle/Characters/TrueProgrammer.cs
@@ -1,7 +1,8 @@
 namespace FinalBattle;
 
-public class TrueProgrammer(string name, bool computerControlled = false) : Character(name, computerControlled)
+public class TrueProgrammer(string name, bool computerControlled = false) : Character(computerControlled)
 {
+  public override string Name { get; } = name;
   public override int MaxHp { get; } = 25;
   public override IAttack Attack { get; } = new Punch();
 }
