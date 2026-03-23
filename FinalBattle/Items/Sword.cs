c8af0f02725f566dcde4c2aff42b7b018f872ca6
Jeffrey Seneff <jeffsnff@gmail.com>
2026-03-22 20:33:21 -0400

    Updates the Attack of Sword to 3

--- FinalBattle/Items/Sword.cs
+++ FinalBattle/Items/Sword.cs
@@ -4,6 +4,6 @@ public class Sword() : Gear()
 {
     public override string Name { get; } = "Sword of Fire";
     public override string Description { get; } = "Sword of fire was created by the original Coder of these lands.";
-    public override int Attack { get; } = 8;
+    public override int Attack { get; } = 3;
     public override GearType Type { get; } = GearType.Sword;
 }
