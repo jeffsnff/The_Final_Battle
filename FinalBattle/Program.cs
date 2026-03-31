a1a6258c50ad6fff363affe301784861629a40da
Jeffrey Seneff <jeffsnff@gmail.com>
2026-03-31 16:32:22 -0400

    Updates Console Title and removes commented out code

--- FinalBattle/Program.cs
+++ FinalBattle/Program.cs
@@ -5,20 +5,8 @@ class Program
   static void Main(string[] args)
   {
     Console.SetWindowSize(100,100);
-    
-    Console.WriteLine("The Final Battle");
-
+    Console.Title = "The Final Battle";
     Game game = new Game();
     game.Run();
-
-    // if (int.TryParse(Console.ReadLine(), out int gameMode))
-    // {
-    //     Game game = new Game(gameMode);
-    //     game.Run();
-    // }
-    // else
-    // {
-    //     Game game = new Game(1);
-    // }
   }
 }
